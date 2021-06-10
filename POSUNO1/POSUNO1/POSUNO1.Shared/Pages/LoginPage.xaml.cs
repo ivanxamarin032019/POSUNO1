6aabd2f25c487417e75161c3134a4a9f14ac8cd1
Ivan Xamarin <ivanxamarin032019@hotmail.com>
2021-06-10 13:57:17 +0200

    TErminado el punto 8 del Word, pero con errores  Popup' is an ambiguous reference between ...

--- POSUNO1/POSUNO1/POSUNO1.Shared/Pages/LoginPage.xaml.cs
+++ POSUNO1/POSUNO1/POSUNO1.Shared/Pages/LoginPage.xaml.cs
@@ -1,17 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+﻿using POSUNO1.Components;
+using POSUNO1.Helpers;
+using POSUNO1.Models;
+using System;
+using System.Threading.Tasks;
+using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -25,8 +19,78 @@ namespace POSUNO1.Pages
         public LoginPage()
         {
             this.InitializeComponent();
+            EmailTextBox.Text = "ivan@yopmail.com";
+            PasswordPasswordBox.Password = "123456";
+
         }
 
+        private async void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            bool isValid = await ValidForm();
+            if (!isValid)
+            {
+                return;
+            }
 
+            Loader loader = new Loader("Por favor espere...");
+            loader.Show();
+            Response response = await ApiService.LoginAsync(new LoginRequest
+            {
+                Email = EmailTextBox.Text,
+                Password = PasswordPasswordBox.Password
+            });
+            loader.Close();
+
+            MessageDialog messageDialog;
+            if (!response.IsSuccess)
+            {
+                messageDialog = new MessageDialog(response.Message, "Error");
+                await messageDialog.ShowAsync();
+                return;
+            }
+
+            User user = (User)response.Result;
+            if (user == null)
+            {
+                messageDialog = new MessageDialog("Usuario o Contraseña incorrectos.", "Error");
+                await messageDialog.ShowAsync();
+                return;
+            }
+
+            Frame.Navigate(typeof(MainPage), user);
+        }
+
+        private async Task<bool> ValidForm()
+        {
+            MessageDialog messageDialog;
+
+            if (string.IsNullOrEmpty(EmailTextBox.Text))
+            {
+                messageDialog = new MessageDialog("Debes ingresar tu email.", "Error");
+                await messageDialog.ShowAsync();
+                return false;
+            }
+
+            if (!RegexUtilities.IsValidEmail(EmailTextBox.Text))
+            {
+                messageDialog = new MessageDialog("Debes ingresar un email válido.", "Error");
+                await messageDialog.ShowAsync();
+                return false;
+            }
+
+            if (PasswordPasswordBox.Password.Length < 6)
+            {
+                messageDialog = new MessageDialog("Debes ingresar tu contraseña de al menos (6) caracteres.", "Error");
+                await messageDialog.ShowAsync();
+                return false;
+            }
+
+            return true;
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
     }
 }
